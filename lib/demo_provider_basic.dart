faa96f44a4d92c28bfdf59c6e99da391da520a51
phamtanphat <phatdroid94@gmail.com>
2022-11-01 19:30:09 +0700

    basic provider

--- lib/demo_provider_basic.dart
+++ lib/demo_provider_basic.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 class DemoProviderBasicPage extends StatefulWidget {
 
   @override
@@ -8,6 +9,53 @@ class DemoProviderBasicPage extends StatefulWidget {
 class _DemoProviderBasicPageState extends State<DemoProviderBasicPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Demo Provider Basic"),
+      ),
+      body: Container(
+        child: Center(
+          child: Provider(
+            create: (context) => 10,
+            child: OngBa(
+              child: Chame(),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
+
+class OngBa extends StatelessWidget {
+
+  Widget child;
+  
+  OngBa({required this.child});
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: [
+          Text("Ong ba"),
+          child
+        ],
+      ),
+    );
+  }
+}
+
+class Chame extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    int value = Provider.of(context);
+    return Container(
+      child: Text("Cha me, value: $value"),
+    );
+  }
+}
+
+
+
