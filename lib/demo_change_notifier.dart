b4dd8860045e8915d8135f0e760b268feed5ed94
phamtanphat <phatdroid94@gmail.com>
2022-11-03 18:24:02 +0700

    changenotifier

--- lib/demo_change_notifier.dart
+++ lib/demo_change_notifier.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+class Count extends ChangeNotifier {
+  int value = 0;
+
+  void increaseValue() {
+    value += 1;
+    notifyListeners();
+  }
+}
+
 class DemoChangeNotifierPage extends StatefulWidget {
 
   @override
@@ -8,29 +18,40 @@ class DemoChangeNotifierPage extends StatefulWidget {
 class _DemoChangeNotifierPageState extends State<DemoChangeNotifierPage> {
   @override
   Widget build(BuildContext context) {
+    print("Parent Build");
     return Scaffold(
       appBar: AppBar(
         title: Text("Demo Change Notifier"),
       ),
       body: Container(
         constraints: BoxConstraints.expand(),
-        child: Center(child: ChangeNotifierContainer()),
+        child: Center(
+            child: ChangeNotifierProvider(
+                create: (context) => Count(),
+                child: CountContainer()
+            )
+        ),
       ),
     );
   }
 }
 
-class ChangeNotifierContainer extends StatelessWidget {
+class CountContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Count count = Provider.of(context);
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Text("Count "),
+        Consumer<Count>(
+            builder: (context, count, child){
+              return Text("Count ${count.value}");
+            },
+        ),
         ElevatedButton(
             onPressed: () {
-
+                count.increaseValue();
             },
             child: Text("Increase")
         )
@@ -40,10 +61,3 @@ class ChangeNotifierContainer extends StatelessWidget {
 }
 
 
-class Count {
-  int value = 0;
-
-  void increaseValue() {
-    value += 1;
-  }
-}
