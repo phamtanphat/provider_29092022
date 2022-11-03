b2e2b8079cc37c9a0d5c4b87a2e160ca5d05a602
phamtanphat <phatdroid94@gmail.com>
2022-11-03 18:49:48 +0700

    setup proxy

--- lib/demo_proxy_provider.dart
+++ lib/demo_proxy_provider.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_29092022/demo_change_notifier.dart';
 class Count {
   int value = 0;
 
@@ -31,5 +34,42 @@ class CountRepository {
 
   void handlerCountRepository() {
     countService?.handlerCountService();
+  }
+}
+
+
+class DemoProxyProvider extends StatefulWidget {
+
+  @override
+  State<DemoProxyProvider> createState() => _DemoProxyProviderState();
+}
+
+class _DemoProxyProviderState extends State<DemoProxyProvider> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Demo Proxy Provider"),
+      ),
+      body: MultiProvider(
+        providers: [
+          Provider(create: (context) => Count()),
+          ProxyProvider<Count, CountService>(
+              create: (context) => CountService(),
+              update: (context, count, service) {
+                service?.updateCount(count);
+                return service ?? CountService();
+              }
+          ),
+          ProxyProvider<CountService, CountRepository>(
+              create: (context) => CountRepository(),
+              update: (context, service, repository) {
+                repository?.updateCountService(service);
+                return repository ?? CountRepository();
+              }
+          ),
+        ],
+      )
+    );
   }
 }
